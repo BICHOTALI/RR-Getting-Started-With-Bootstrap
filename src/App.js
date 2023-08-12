015415b7c73e728ac9cd1e406ea277820e55b6d6
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-08-12 13:31:17 -0400

    react-bootstrap; button and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
-
+import InfoButton from './Button';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <h1>Bootstrap Website</h1>
+      <InfoButton />
      
     </div>
   );
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function InfoButton() {
+  return (
+    <>
+      <Button variant="outline-info">Button</Button>{' '}
+    </>
+  );
+}
+
+export default InfoButton;
